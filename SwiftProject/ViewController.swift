39620d4ff3e0840ccb995d967688f8d382e7730c
Miguel Angel Jimenez Melendez <migueajm9724@MacBook-Pro-de-Miguel.local>
2020-04-23 23:36:12 -0500

    Comienzo del proyecto, primeras view (Diseño)

---
+++ SwiftProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftProject
+//
+//  Created by Miguel Angel Jimenez Melendez on 4/23/20.
+//  Copyright © 2020 Miguel Angel Jimenez Melendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
