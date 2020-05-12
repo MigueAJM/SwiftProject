e0821916c6775f92bebbcb7646241ca19965f8a8
AetherStark <fran.16980405@gmail.com>
2020-05-12 14:15:28 -0500

    Actualizacion de Web Services y Base de datos

--- WebServices&SQL/securityapp.sql
+++ WebServices&SQL/securityapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 12-05-2020 a las 21:12:16
+-- Tiempo de generación: 12-05-2020 a las 21:13:58
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.30
 
