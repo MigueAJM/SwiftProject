2a1b959792fe1ebb71bbd6375a128849957699a6
AetherStark <fran.16980405@gmail.com>
2020-05-12 14:12:47 -0500

    Actualizacion de Web Services

--- WebServices&SQL/securityapp.sql
+++ WebServices&SQL/securityapp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 06-05-2020 a las 21:45:18
+-- Tiempo de generación: 12-05-2020 a las 21:12:16
 -- Versión del servidor: 10.4.11-MariaDB
 -- Versión de PHP: 7.2.30
 
@@ -30,12 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `reportes` (
   `idreporte` int(11) NOT NULL,
   `ubicacion` text NOT NULL,
-  `fecha` date NOT NULL,
+  `fecha` text NOT NULL,
   `descripcion` text NOT NULL,
   `Titulo` text NOT NULL,
   `puntuacion` int(11) NOT NULL,
   `corrUsr` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `reportes`
+--
+
+INSERT INTO `reportes` (`idreporte`, `ubicacion`, `fecha`, `descripcion`, `Titulo`, `puntuacion`, `corrUsr`) VALUES
+(3, 'defwefewfewfwef', '2020/05/12', 'Esta es una prueba mamamlona', 'PRUEBA', 5, 'prueba@hotmail.com'),
+(4, 'Celaya', 'Hoy', 'frferferfefefefefefefeefefefe', 'Prueba 2', 5, 'prueba2@hotmail.com');
 
 -- --------------------------------------------------------
 
@@ -51,6 +59,16 @@ CREATE TABLE `usuarios` (
   `sexo` char(1) NOT NULL,
   `telefono` varchar(10) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`foto`, `nombre`, `correo`, `fechaNac`, `sexo`, `telefono`) VALUES
+('https://asa', 'Alex Lora', 'al@hotmail.com', '0000-00-00', 'H', 'prueba@hot'),
+('https://', 'Francisco', 'fran.16980405@gmail.com', '2020-05-12', 'H', '1111111111'),
+('https://', 'prueba 2', 'prueba2@hotmail.com', '2020-05-12', 'M', '5555555555'),
+('https://', 'Prueba', 'prueba@hotmail.com', '2020-05-12', 'H', '333333333');
 
 --
 -- Índices para tablas volcadas
@@ -77,7 +95,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `reportes`
 --
 ALTER TABLE `reportes`
-  MODIFY `idreporte` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `idreporte` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
